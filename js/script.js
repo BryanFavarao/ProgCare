5679a953cc874e688e163b2551392a43015227cc
RaphaelSilvaL <rapha.43211@gmail.com>
2023-06-02 16:42:48 -0300

    progcare

---
+++ js/script.js
@@ -0,0 +1,52 @@
+//animação do scroll
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  
+  const target = document.querySelectorAll('[data-anime]');
+  const animationClass = 'animate';
+  
+  function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+      if((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass);
+      } else {
+        element.classList.remove(animationClass);
+      }
+    })
+  }
+  
+  animeScroll();
+  
+  if(target.length) {
+    window.addEventListener('scroll', debounce(function() {
+      animeScroll();
+    }, 200));
+  }
+
+
+
+
+  //trocar de tela, caso contato dê certo
+  function passar() {
+    window.location.href = "index.html"
+  }
+
+
+  function unloadScrollBars() {
+    document. documentElement. style. overflow = 'hidden';
+    document. body. scroll = "no";
+    }
+    
